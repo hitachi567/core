5560e1923c5202722c08797ab9047fe02f440f86
hitachi567 <abidai790@gmail.com>
2022-01-02 18:08:33 +0300

    remove init function

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,3 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import helmet from 'helmet';
-
-export default function init(cookieSecret?: string) {
-    let app = express();
-    app.use(helmet());
-    app.use(cors());
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    app.use(cookieParser(cookieSecret));
-}
 
 export * from './interfaces/index';
 export * from './services/index';
