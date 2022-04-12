51d6a8f970a6407f88459a0691a4c67a6b1cd7c2
hitachi567 <abidai790@gmail.com>
2022-04-12 19:08:29 +0300

    validation class was added

---
+++ src/services/validation.ts
@@ -0,0 +1,44 @@
+import {
+    validate,
+    ValidationError,
+    ValidatorOptions
+} from 'class-validator';
+
+export default class Validation {
+
+    static async validate(
+        object: object, validatorOptions?: ValidatorOptions
+    ): Promise<string[]> {
+        let options: ValidatorOptions = {
+            validationError: {
+                target: false,
+                value: false
+            },
+            ...validatorOptions
+        }
+
+        return this.handle(
+            await validate(object, options)
+        );
+    }
+
+    protected static handle(results: ValidationError[]): string[] {
+        let errors: string[] = [];
+
+        for (const result of results) {
+            if (result?.constraints) {
+                for (const key in result.constraints) {
+                    const error = result.constraints[key];
+                    if (Object.prototype.hasOwnProperty.call(
+                        result.constraints, key
+                    )) {
+                        errors.push(error);
+                    }
+                }
+            }
+        }
+
+        return errors;
+    }
+
+}
